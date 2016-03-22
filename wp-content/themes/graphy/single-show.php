fa9af6659058076e9cf58c6430ee1d0384223a5e
dev <dev@freeformportland.org>
2016-03-21 21:19:29 -0700

    Add make new playlist button to show page

--- wp-content/themes/graphy/single-show.php
+++ wp-content/themes/graphy/single-show.php
@@ -2,12 +2,17 @@
 /**
  * @package Graphy
  */
-?>
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-    <h2>TEST-PSL</h2>
+
+
 		<?php graphy_header_meta(); ?>
 		<?php if ( has_post_thumbnail() ): ?>
 		<div class="post-thumbnail"><?php the_post_thumbnail(); ?></div>
@@ -20,3 +25,22 @@
 	</div><!-- .entry-content -->
 	<?php graphy_footer_meta(); ?>
 </article><!-- #post-## -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<? // if dj is logged in, show the show id and a link to create a playlist ?>
+<?php if (is_user_logged_in ()) {
+?>
+<?php
+  $custom_fields = get_post_custom();
+  $showID = $custom_fields['id'][0];
+  echo "KFFP ID: ". $showID."<br/>";
+?>
+
+<a href='http://kffp.rocks/api/newSetlist/<?= $showID ?>' target='_blank'>Make New Playlist</a>
+
+<?php } ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
