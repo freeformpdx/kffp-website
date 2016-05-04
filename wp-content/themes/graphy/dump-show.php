4084b5211f01ded6e2b0f10f0ca31decaf26b4d4
dev <dev@freeformportland.org>
2016-05-04 07:31:38 -0700

    show schedule dump @ /schedule-dump/

--- wp-content/themes/graphy/dump-show.php
+++ wp-content/themes/graphy/dump-show.php
@@ -27,24 +27,33 @@ $args=array(
   'caller_get_posts'=> 1
 );
 
+$allShows = array();
+
 $my_query = null;
 $my_query = new WP_Query($args);
 if( $my_query->have_posts() ) {
-  while ($my_query->have_posts()) : $my_query->the_post(); ?>
+  while ($my_query->have_posts()) : $my_query->the_post();
+
+    // make show object && add it to $allShows
+    $showData = array(
+      "showID" => get_post_meta($post->ID, 'id', true),
+      "title" => get_the_title(),
+      "startDay" => get_post_meta($post->ID, 'start_day', true),
+      "startHour" => get_post_meta($post->ID, 'start_hour', true),
+      "endDay" => get_post_meta($post->ID, 'end_day', true),
+      "endHour" => get_post_meta($post->ID, 'end_hour', true),
+    );
+
+    $allShows[] = $showData;
 
 
-
-    <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-
-
-
-
-
-    <?php
   endwhile;
 }
+
 wp_reset_query();  // Restore global post data stomped by the_post().
 ?>
+
+<pre><?php echo json_encode($allShows); ?></pre>
 
 
 </body>
